915ad8280584026ac1cf4d35dcc1ae95a4e08da2
Xavier Piquer <xavi@metadev.net>
2010-10-11 14:33:30 +0200

    Added class to get trees

---
+++ tree.class.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * zPhpExt. Easy and simplified ExtJS UI Wrapper
+ *
+ * This software allows you to create ExtJs user interfaces using only PHP code.
+ * @author Xavier Piquer <xavi@metadev.net>
+ * @version 1.0
+ * @package zPhpExt
+ */
+
+namespace zPhpExt;
+
+/**
+ * Main class. Extjs viewport construction.
+ *
+ * @package zPhpExt
+ * @subpackage classes
+ */
+
+class tree extends panel
+{
+    /**
+     * Public var. stores object type
+     * @access public
+     * @var string
+     */
+    public $itemType = 'tree';
+
+
+}
