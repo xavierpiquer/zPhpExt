cf9275715289d5a86e67984a1689489460c2f112
Xavier Piquer <xavi@metadev.net>
2010-10-11 14:33:07 +0200

    Added class to get grids

---
+++ grid.class.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * zPhpExt. Easy and simplified ExtJS UI Wrapper
+ *
+ * This software allows you to create ExtJs user interfaces using only PHP code.
+ * @author Xavier Piquer <xavi@metadev.net>
+ * @version 1.0
+ * @package zPhpExt
+ */
+
+namespace zPhpExt;
+
+/**
+ * Main class. Extjs viewport construction.
+ *
+ * @package zPhpExt
+ * @subpackage classes
+ */
+
+class grid extends panel
+{
+    /**
+     * Public var. stores object type
+     * @access public
+     * @var string
+     */
+    public $itemType = 'grid';
+
+    
+}
